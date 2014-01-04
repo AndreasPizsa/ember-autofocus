cf4235c24c7d5facbf593e71cd7eacca3c60cb73
Andreas Pizsa <apizsa@andreasizsasair.home>
2014-01-04 01:15:14 +0100

    Adding Grunt

--- src/ember-autofocus.js
+++ src/ember-autofocus.js
@@ -1,49 +1,48 @@
 ;(function(){
-	// my own namespace = my own clutter
-	window.AndreasPizsa = window.AndreasPizsa || {};
-	AndreasPizsa.Ember = AndreasPizsa.Ember || {};
+    // my own namespace = my own clutter
+    window.AndreasPizsa = window.AndreasPizsa || {};
+    AndreasPizsa.Ember = AndreasPizsa.Ember || {};
 
-	AndreasPizsa.Ember.ViewAutofocus = Ember.View.extend({
-	  attributeBindings	: ['on','scroll'],
-	  useScheduling		: true,
-	  on				: null,
-	  scroll			: false,
-	  useNativeAutofocus: ('autofocus' in document.createElement('input')),
+    AndreasPizsa.Ember.ViewAutofocus = Ember.View.extend({
+      attributeBindings : ['on','scrolling'],
+      useScheduling     : true,
+      on                : null,
+      scrolling         : false,
+      useNativeAutofocus: ('autofocus' in document.createElement('input')),
 
-	  scheduleAutofocus: function() {
-	  	if(this.useNativeAutofocus) return;
+      scheduleAutofocus: function() {
+        if(this.useNativeAutofocus) return;
 
-	    if (this.useScheduling) {
-	      Ember.run.scheduleOnce('afterRender', this, this.autofocus);
-	    } else {
-	      this.autofocus();
-	    }
-	  }.on('didInsertElement'),
+        if (this.useScheduling) {
+          Ember.run.scheduleOnce('afterRender', this, this.autofocus);
+        } else {
+          this.autofocus();
+        }
+      }.on('didInsertElement'),
 
-	  preventScrolling : function() {
-	  	if(!this.scroll) {
-	  		window.scrollTo(0,0);
-	  		window.setTimeout(function(){window.scrollTo(0,0)},100);
-	  	}
-	  }.on('didInsertElement'),
+      preventScrolling : function() {
+        if(!this.scrolling) {
+            window.scrollTo(0,0);
+            window.setTimeout(function(){window.scrollTo(0,0);},100);
+        }
+      }.on('didInsertElement'),
 
-	  autofocus: (function() {
-	    var scrollX, scrollY, selector;
-	    selector = this.on;
-	    if (selector === null) {
-	      selector = 'input[autofocus],textarea[autofocus],button[autofocus]';
-	      if (Em.$(selector).length === 0) {
-	        selector = 'input';
-	      }
-	    }
-	    scrollX = window.scrollX, scrollY = window.scrollY;
-	    Em.$(selector).first().select().focus();
-	    window.scrollTo(scrollX, scrollY);
-	  })
-	});
+      autofocus: (function() {
+        var selector = this.on;
+        if (selector === null) {
+          selector = 'input[autofocus],textarea[autofocus],button[autofocus]';
+          if (Em.$(selector).length === 0) {
+            selector = 'input';
+          }
+        }
+        var scrollX = window.scrollX, scrollY = window.scrollY;
+        Em.$(selector).not(':hidden').first().select().focus();
+        window.scrollTo(scrollX, scrollY);
+      })
+    });
 
-	Ember.Handlebars.helper('autofocus', AndreasPizsa.Ember.ViewAutofocus);
-	Ember.TextField.reopen({
-	  attributeBindings: ['autofocus']
-	});
+    Ember.Handlebars.helper('autofocus', AndreasPizsa.Ember.ViewAutofocus);
+    Ember.TextField.reopen({
+      attributeBindings: ['autofocus']
+    });
 }());
